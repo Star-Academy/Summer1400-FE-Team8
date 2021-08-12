47d8db8bb02482e5809e1489e43e05fcc408c2e2
bolnik <amir.bolnik@gmail.com>
2021-08-12 19:11:13 +0430

    move commons to separate folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { SearchBoxComponent } from './components/common/search-box/search-box.component';
 import { LoginSignupComponent } from './components/login-signup/login-signup.component';
 import { LoginComponent } from './components/login-signup/login/login.component';
 import { SignupComponent } from './components/login-signup/signup/signup.component';
 import { PlayerComponent } from './components/player/player.component';
+import { PlaylistComponent } from './components/playlist/playlist.component';
+import { PlaylistsComponent } from './components/playlists/playlists.component';
+import { MusicSearchComponent } from './components/music-search/music-search.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { PlayerComponent } from './components/player/player.component';
     LoginSignupComponent,
     LoginComponent,
     SignupComponent,
-    PlayerComponent
+    PlayerComponent,
+    PlaylistComponent,
+    PlaylistsComponent,
+    MusicSearchComponent,
+    EditProfileComponent
   ],
   imports: [
     BrowserModule,
