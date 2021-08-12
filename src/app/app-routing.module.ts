7e53d05cd432346bc459060fa7a0fa3209a96f6a
bolnik <amir.bolnik@gmail.com>
2021-08-12 15:06:11 +0430

    needed files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {HomepageComponent} from './components/homepage/homepage.component';
+import {LoginSignupComponent} from './components/login-signup/login-signup.component';
+import {LoginComponent} from './components/login-signup/login/login.component';
+import {PlayerComponent} from './components/player/player.component';
+const routes: Routes = [
+  { path:'',component: HomepageComponent},
+  { path:'user',component: LoginSignupComponent , children : [
+      {path:'login',component: LoginComponent}
+  ]},
+  { path:'player',component: PlayerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { LoginSignupComponent } from './components/login-signup/login-signup.component';
+import { LoginComponent } from './components/login-signup/login/login.component';
+import { SignupComponent } from './components/login-signup/signup/signup.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchBoxComponent,
+    LoginSignupComponent,
+    LoginComponent,
+    SignupComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
