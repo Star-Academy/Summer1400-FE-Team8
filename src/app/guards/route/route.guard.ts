2db6dd764cda56a8340c5534de14ec6ed2077a44
bolnik <amir.bolnik@gmail.com>
2021-08-14 00:11:29 +0430

    route authorization guard

---
+++ src/app/guards/route/route.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+@Injectable({
+  providedIn: 'root'
+})
+export class RouteGuard implements CanActivate {
+  constructor(private authService: AuthService,private router: Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+   if(!this.authService.isLogged()){
+      this.router.navigate(['/']);
+      return false
+   }
+      return true;
+  }
+  
+}
