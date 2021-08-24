b14fbf6eb9676d7214a88f7537c78a1efb0c45f9
bolnik <amir.bolnik@gmail.com>
2021-08-24 22:07:42 +0430

    test : music-search component

--- src/app/components/music-search/music-search.component.spec.ts
+++ src/app/components/music-search/music-search.component.spec.ts
@@ -1,27 +1,42 @@
+import { LocationStrategy } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockLocationStrategy } from '@angular/common/testing';
+import { ComponentFixture, fakeAsync, TestBed , inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { routes } from 'src/app/app-routing.module';
+import { Song } from 'src/app/interfaces/interfaces';
+import { SongService } from 'src/app/services/song/song.service';
 import { UrlService } from 'src/app/services/url/url.service';
 import { SearchBoxComponent } from '../common/search-box/search-box.component';
 
 import { MusicSearchComponent } from './music-search.component';
 
+class mockedUrlService{
+  setParams(){
+
+  }
+  getParams(){
+
+  }
+}
 
 describe('MusicSearchComponent', () => {
   let component: MusicSearchComponent;
-  let service : UrlService;
   let fixture: ComponentFixture<MusicSearchComponent>;
-
-
-
-  
+  let router: ActivatedRoute;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ MusicSearchComponent , SearchBoxComponent ],
       imports:[
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(routes),
         HttpClientTestingModule
+      ],
+      providers:[
+        { provide: LocationStrategy, useClass: MockLocationStrategy},
+        { provide: UrlService, useClass: mockedUrlService},
       ]
     })
     .compileComponents();
@@ -31,22 +46,71 @@ describe('MusicSearchComponent', () => {
     fixture = TestBed.createComponent(MusicSearchComponent);
     component = fixture.componentInstance;
     spyOn(component, 'handleReload').and.returnValue();
-    // spyOn(window.location, 'search');
-    service = new UrlService();
+    router = TestBed.get(ActivatedRoute)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should create',inject([SongService],(songService:SongService)=>{
+    const songs: Song[] = [{
+      id:'1',
+      name:'name1',
+      artist:'artist1',
+    }]
+    spyOn(songService, 'getAllSongs').and.callFake(() => {
+      const res = {songs:songs}
+      return of(res as any);
+    });
+    spyOn(songService, 'postSongsPage').and.callFake(() => {
+        return of(songs)
+    });
+    component.ngAfterViewInit();
+    fixture.detectChanges();
+    expect(songService.getAllSongs).toHaveBeenCalled()
+    expect(songService.postSongsPage).toHaveBeenCalled()
+  }));
+  it('should create',inject([SongService],(songService:SongService)=>{
+    const songs: Song[] = [{
+      id:'1',
+      name:'name1',
+      artist:'artist1',
+    }]
+    spyOn(songService, 'postSongsFind').and.callFake(() => {
+      const res = {songs:songs}
+      return of(res as any);
+  });
+  router.snapshot.queryParams.searched = 'www'  
+  component.ngAfterViewInit();
+  fixture.detectChanges();
+  expect(songService.postSongsFind).toHaveBeenCalled()
+  }));
+  it('should create', fakeAsync(() => {
+    const button = fixture.debugElement
+    .query(By.css('.search-pagination-container button')).nativeElement;
+    spyOn(component, 'handleBackForwardBtns').and.callThrough();
+    button.click();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.handleBackForwardBtns).toHaveBeenCalled();
+    })
+  }));
+  it('should create', fakeAsync(() => {
+    const input = fixture.debugElement
+    .query(By.css('.form-order input[id="desc"]')).nativeElement;
+    spyOn(component, 'handleChangeFilter').and.callThrough();
+    input.click();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(component.handleChangeFilter).toHaveBeenCalled();
+    })
+  }));
   it('should create', () => {
-    // const query = fixture.debugElement.query(By.css('button.search-pagination-forward'));
-    // const btnElm: HTMLDivElement = query.nativeElement;
-    // spyOn(service, 'setParams');
-    // btnElm.click();
-    // fixture.detectChanges();
-    // fixture.whenStable().then(() => {
-    //   expect(service.setParams).toHaveBeenCalled();
-    // });
+    component.handlePagination(100, 'link-all', '1');
+    fixture.detectChanges();
+    const link = fixture.debugElement.query(By.css('.link-all'))
+    .nativeElement;
+    expect(link.classList).not.toContain('display-none');
   });
 });
